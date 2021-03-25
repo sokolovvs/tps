797d85e9708fa55acbfe1a6ace90d3103f6d0638
Vyacheslav Sokolov <vycheslavsokolov2013@gmail.com>
2021-03-25 21:39:19 +0600

    implemented get topics by text

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask
+from flask import request
+from bertopic import BERTopic
+import json
 
 app = Flask(__name__)
 
+topic_model = BERTopic()
+topic_model.load("model")
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+
+@app.route('/topics')
+def get_topics_by_text():
+
+    return json.dumps(topic_model.find_topics(request.args.get("text")))
 
 
 if __name__ == '__main__':
