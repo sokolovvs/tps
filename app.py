59820b29a36b10c18bbc90d82e532d72f3d0a707
Vyacheslav Sokolov <vycheslavsokolov2013@gmail.com>
2021-03-30 19:39:03 +0600

    fixed app.py

--- app.py
+++ app.py
@@ -11,15 +11,25 @@ topic_model.load("model")
 
 @app.route('/api/v1/topics')
 def get_topics_by_text():
+    req = {"public_id": request.headers.get('TPS_PUBLIC_ID', ""),
+           "secret": request.headers.get("TPS_SECRET_KEY", ""),
+           "text": request.args.get("text", "")}
+
     try:
-        ClientService.check_access(request=request.args)
-    except Exception as e:
+        ClientService.check_access(request=req)
+    except AccessDeniedException as e:
         return jsonify({"message": str(e)}), 403
+    except Exception as e:
+        return jsonify({"message": str(e)}), 500
 
     return jsonify(topic_model.find_topics(request.args.get("text")))
 
 
 if __name__ == '__main__':
+    import logging
+
+    logging.basicConfig(filename='error.log', level=logging.DEBUG)
+
     app.run(host=get_parameter_by_key("TPS_HOST"),
             port=get_parameter_by_key("TPS_PORT"),
             debug=get_parameter_by_key("FLASK_DEBUG"))
