3a4ad26db68442be86511e0675f4b89966dea50c
Vyacheslav Sokolov <vycheslavsokolov2013@gmail.com>
2021-04-03 14:13:23 +0600

    changed log stream file -> std

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, jsonify, request
-from settings import get_parameter_by_key
-from services.client_service import ClientService
-from exceptions.access_denied_exception import AccessDeniedException
 from bertopic import BERTopic
+from flask import Flask, jsonify, request
+
+from exceptions.access_denied_exception import AccessDeniedException
+from services.client_service import ClientService
+from settings import get_parameter_by_key
 
 app = Flask(__name__)
 topic_model = BERTopic()
@@ -26,10 +27,6 @@ def get_topics_by_text():
 
 
 if __name__ == '__main__':
-    import logging
-
-    logging.basicConfig(filename='error.log', level=logging.DEBUG)
-
     app.run(host=get_parameter_by_key("TPS_HOST"),
             port=get_parameter_by_key("TPS_PORT"),
             debug=get_parameter_by_key("FLASK_DEBUG"))
